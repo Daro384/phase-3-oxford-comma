f7a687b9788e3149e4351616c546d3c38ec65264
Joseph <joffan98@gmail.com>
2022-10-31 16:40:30 -0500

    commit tree 7c03cad2cbe0013f76d3dfe7ac4446237562e7ef

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length > 2
+        array[0...-1].join(", ") + ", and " + array[-1] 
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+        array[0]
+    end
+        
 end
+
+puts oxford_comma(["1", "5", "20"])
